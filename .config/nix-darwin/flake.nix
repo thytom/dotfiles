d8227ab6952ae04d6e1e99adb29d58d5cec63f73
Archie Hilton <archie.hilton1@gmail.com>
2025-01-18 12:38:36 +0000

    nix: Fix indentation of application script

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -27,14 +27,14 @@
           pkgs.spotify
         ];
 
- system.activationScripts.postUserActivation.text = ''
-    apps_source="${config.system.build.applications}/Applications"
-    moniker="Nix Trampolines"
-    app_target_base="$HOME/Applications"
-    app_target="$app_target_base/$moniker"
-    mkdir -p "$app_target"
-    ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
-  '';
+      system.activationScripts.postUserActivation.text = ''
+          apps_source="${config.system.build.applications}/Applications"
+          moniker="Nix Trampolines"
+          app_target_base="$HOME/Applications"
+          app_target="$app_target_base/$moniker"
+          mkdir -p "$app_target"
+          ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
+          '';
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";

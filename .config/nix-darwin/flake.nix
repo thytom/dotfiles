989448c70e6f69c489c281a4cfc0fb31ed2626c0
Archie Hilton <archie.hilton1@gmail.com>
2025-01-22 23:17:38 +0000

    nix: Clean up the flake

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -1,107 +1,116 @@
 {
-  description = "Example nix-darwin system flake";
+    description = "Example nix-darwin system flake";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-  };
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        nix-darwin.url = "github:LnL7/nix-darwin/master";
+        nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, config, ... }: {
-        nixpkgs.config.allowUnfree = true;
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = with pkgs; [ 
-          vim
-          neovim
-          tmux
-          mkalias
-          fzf
-          ripgrep
-          cmake
-          ninja
-          htop
-          rsync
-          starship
-          nmap
+    outputs = inputs@{ self, nix-darwin, nixpkgs }:
+        let
+        configuration = { pkgs, config, ... }: {
 
-          (python3.withPackages (ps: with ps; [
-            pip
-            regex
-            requests
-            pyserial
-            pyserial-asyncio
-          ]))
+            nixpkgs.config.allowUnfree = true;
 
-          ffmpeg
+# List packages installed in system profile. To search by name, run:
+# $ nix-env -qaP | grep wget
+            environment.systemPackages = with pkgs; [ 
+                (python3.withPackages (ps: with ps; [
+                                       pip
+                                       regex
+                                       requests
+                                       pyserial
+                                       pyserial-asyncio
+                ]))
 
-          # AVR Programming
-          avrdude
-          pkgsCross.avr.buildPackages.gcc
+# Workflow
+                starship
+                tmux
+                zsh
+                neovim
 
-          # LaTeX
-          texliveFull
+# Utils 
+                ffmpeg
+                vim
+                htop
+                fzf
+                nmap
+                ripgrep
+                rsync
 
-          # Graphical Applications
-          alacritty
-          spotify
-          vlc-bin
-        ];
+# Nix Programming Lanuages
+                nil # Language server: https://github.com/oxalica/nil
 
-      homebrew = {
-          enable = true;
+# General Dev
+                cmake
+                ninja
+
+# AVR
+                avrdude
+                pkgsCross.avr.buildPackages.gcc
+
+# LaTeX
+                texliveFull
+
+# Graphical Applications
+                alacritty
+                spotify
+                vlc-bin
+                ];
+
+            homebrew = {
+                enable = true;
 #onActivation.cleanup = "uninstall";
 
-          taps = [];
-          brews = [];
-          casks = [ 
-           "thunderbird" 
-           "obsidian"
-           "minecraft"
-           "google-chrome"
-          ];
-      };
+                taps = [];
+                brews = [];
+                casks = [ 
+                    "thunderbird" 
+                    "obsidian"
+                    "minecraft"
+                    "google-chrome"
+                ];
+            };
 
-      fonts.packages = with pkgs; [
-          nerd-fonts.go-mono
-          b612
-      ];
+            fonts.packages = with pkgs; [
+                nerd-fonts.go-mono
+                    b612
+            ];
 
-      system.activationScripts.postUserActivation.text = ''
-          apps_source="${config.system.build.applications}/Applications"
-          moniker="Nix Trampolines"
-          app_target_base="$HOME/Applications"
-          app_target="$app_target_base/$moniker"
-          mkdir -p "$app_target"
-          ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
-          '';
+            system.activationScripts.postUserActivation.text = ''
+                apps_source="${config.system.build.applications}/Applications"
+                moniker="Nix Trampolines"
+                app_target_base="$HOME/Applications"
+                app_target="$app_target_base/$moniker"
+                mkdir -p "$app_target"
+                ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
+                '';
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+# Necessary for using flakes on this system.
+            nix.settings.experimental-features = "nix-command flakes";
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+# Enable alternative shell support in nix-darwin.
+# programs.fish.enable = true;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+# Set Git commit hash for darwin-version.
+            system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
+# Used for backwards compatibility, please read the changelog before changing.
+# $ darwin-rebuild changelog
+            system.stateVersion = 5;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+# The platform the configuration will be used on.
+            nixpkgs.hostPlatform = "aarch64-darwin";
+        };
+    in
+    {
+# Build darwin flake using:
+# $ darwin-rebuild build --flake .#defiantly
+        darwinConfigurations."defiantly" = nix-darwin.lib.darwinSystem {
+            modules = [ 
+                configuration 
+            ];
+        };
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."defiantly" = nix-darwin.lib.darwinSystem {
-      modules = [ 
-        configuration 
-      ];
-    };
-  };
 }

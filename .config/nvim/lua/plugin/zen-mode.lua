fdef65f193dff21d91e10a54db4ff486b2e65910
Archie Hilton <archie.hilton1@gmail.com>
2025-04-15 20:41:49 +0100

    nvim: Add zen-mode

---
+++ .config/nvim/lua/plugin/zen-mode.lua
@@ -0,0 +1,12 @@
+return {
+    "folke/zen-mode.nvim",
+    opts = {
+        window = {
+            width = 80,
+            options = {
+                number = false,
+                relativenumber = false,
+            },
+        }
+    }
+}

a4119745ff33dafdc487d54e4cdd5c43ba7509c4
Archie Hilton <archie.hilton1@gmail.com>
2026-04-01 10:48:26 +0100

    nvim: Update conform settings

--- .config/nvim/lua/plugin/conform.lua
+++ .config/nvim/lua/plugin/conform.lua
@@ -2,8 +2,14 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
+      c = { "clang-format" },
+      cpp = { "clang-format" },
       lua = { "stylua" },
       nix = { "nixfmt" },
+    },
+
+    default_format_opts = {
+      lsp_format = "fallback",
     },
   },
   config = function(_, opts)
@@ -28,5 +34,14 @@ return {
     vim.keymap.set("v", "<leader>cf", format_range, { desc = "Format selection with Conform" })
 
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function()
+        local formatters = require("conform").list_formatters_for_buffer()
+        if #formatters == 0 then
+          vim.bo.formatexpr = ""
+        end
+      end,
+    })
   end,
 }

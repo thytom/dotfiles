edc71c15bf47dda8eb0af77ee05062bc3fc5ec66
Archie Hilton <archie.hilton1@gmail.com>
2026-05-07 21:48:40 +0100

    nix: Re-key secrets

--- .config/nix/secrets/keys.nix
+++ .config/nix/secrets/keys.nix
@@ -1,4 +1,5 @@
 {
     defiantly = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL4WR5WBeOJpFRR3xtOYpAgHu4cehR/krFw49Re+UEiR archie@defiantly.local";
     archies-home-worklab = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID3DBx9E6WijQgzoIE9JSk3MX4cZGZ5o6zzpSxZlcAe3 root@archies-home-worklab";
+    backup_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPOyc2jVy8TGbZXalLb4Wn1unJbd9UtYQbaEqmu+qxtI archie_backup_key";
 }
